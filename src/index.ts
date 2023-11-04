2ccecfacb7118d1b28a135dc3d13bd561449321a
Drew Harris <harrisd@smu.edu>
2023-11-04 18:51:17 -0500

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+import cors from "@elysiajs/cors";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(cors())
+
+  .get("/", () => {
+    return "Hello Elysia";
+  })
+  .post("/hack", ({ body }) => {
+    console.log("Body", body);
+    // Parse body as cookies
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
